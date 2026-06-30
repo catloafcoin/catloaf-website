b81d4f0b42bdb4fe1b27cf1582e3af1f91b9a8de
catloafcoin <catloafcoin@gmail.com>
2026-06-30 12:52:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,28 +2,10 @@ function copyCA() {
     const ca = "GEEQMeTNRYvQKBHZo2jEaHd7j93AZugmSa6UsktJpump";
 
     navigator.clipboard.writeText(ca).then(() => {
-        alert("✅ Contract Address copied!");
-    }).catch(() => {
-        alert("Copy failed. Please copy it manually.");
+        alert("Contract Address copied!");
     });
 }
 
-const cards = document.querySelectorAll(".card");
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.style.opacity = "1";
-            entry.target.style.transform = "translateY(0)";
-        }
-    });
-}, {
-    threshold: 0.2
-});
-
-cards.forEach((card) => {
-    card.style.opacity = "0";
-    card.style.transform = "translateY(40px)";
-    card.style.transition = "all 0.8s ease";
-    observer.observe(card);
-});
+window.onload = function () {
+    console.log("$CLOAF website loaded.");
+};
