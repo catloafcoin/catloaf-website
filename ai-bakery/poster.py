7e2596850dd21e0c3517ad1b27951ea6b199a8b5
catloafcoin <catloafcoin@gmail.com>
2026-07-02 01:47:25 +0530

    Update poster.py

--- ai-bakery/poster.py
+++ ai-bakery/poster.py
@@ -1,5 +1,9 @@
 from scheduler import get_queue, remove_first, should_post_now
-from modules import send_telegram
+from modules import send_telegram, send_poll
+import os
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 def process_queue():
     queue = get_queue()
